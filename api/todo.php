b45cf95cc009c9b6f600a8ff8e34e4e67fb68908
Ramiro <mostlyphpsoftware@gmail.com>
2025-09-14 23:14:36 -0600

    agregando ruta absoluta ala configuracion de conexion con la base de adatos

--- api/todo.php
+++ api/todo.php
@@ -3,7 +3,7 @@
 // ----------------------------------------------------
 // Incluimos el archivo de configuración
 // ----------------------------------------------------
-require_once __DIR__ . '/../config.php';
+require_once '/home/timeziej/syntax/sanctuary.com/p4ws/config.php';
 
 class Database {
     private PDO $pdo;
