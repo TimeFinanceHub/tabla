e29b030f00ef5c25cfc1f5c8801330861a0801ad
Ramiro <mostlyphpsoftware@gmail.com>
2025-09-14 23:27:32 -0600

    corrigiendo dbname

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 
 // Credenciales para la base de datos MySQL
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'todolist');
+define('DB_NAME', 'timeziej_todolist');
 define('DB_USER', 'timeziej_admin'); // Asegúrate de cambiar 'root' si usas otro usuario
 define('DB_PASS', 'bXqnk29CZXGE@ad');    // Asegúrate de cambiar '' si tienes una contraseña
 ?>
